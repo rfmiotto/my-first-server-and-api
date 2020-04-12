02b0fae43ee3794a9ea1f98efff6d8f966365b1a
Renato Miotto <renato.fmiotto@gmail.com>
2020-04-12 16:31:35 -0300

    Add insertion/deletion requests to api

--- http/public/scripts.js
+++ http/public/scripts.js
@@ -2,6 +2,20 @@ const ul = document.querySelector("ul")
 const input = document.querySelector("input")
 const form = document.querySelector('form')
 
+async function loadFromServer() {
+    const apiRes = await fetch("http://localhost:3000")
+        .then((data) => data.json())
+        
+    apiRes.urls.map(({name, url}) => addElement({name, url}))
+}
+
+async function removeFromServer({ name, url }) {
+    await fetch(`http://localhost:3000?name=${name}&url=${url}&del=1`)
+}
+
+async function addToServer({ name, url }) {
+    await fetch(`http://localhost:3000?name=${name}&url=${url}`)
+}
 
 function addElement({ name, url }) {
     const li = document.createElement('li')
@@ -21,9 +35,16 @@ function addElement({ name, url }) {
 }
 
 function removeElement(el) {
-    if (confirm('Tem certeza que deseja deletar?'))
+    if (confirm('The selected item will be deleted. Confirm the operation.'))
         el.parentNode.remove()
+        removeFromServer({
+            "name": `${el.parentNode.querySelector("a").innerHTML}`,
+            "url": `${el.parentNode.querySelector("a").href.slice(0, -1)}`
+        })
 }
+
+
+loadFromServer()
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -31,17 +52,18 @@ form.addEventListener("submit", (event) => {
     let { value } = input
 
     if (!value) 
-        return alert('Preencha o campo')
+        return alert('Field cannot be empty.')
 
     const [name, url] = value.split(",")
 
     if (!url) 
-        return alert('formate o texto da maneira correta')
+        return alert('Use comma to separate the name from the URL.')
 
     if (!/^http/.test(url)) 
-        return alert("Digite a url da maneira correta")
+        return alert("URL must start with http://")
 
     addElement({ name, url })
+    addToServer({ name, url })
 
     input.value = ""
 })
